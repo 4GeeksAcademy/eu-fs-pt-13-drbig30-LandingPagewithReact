1f8ce099c1b5877d288c03fc08a26aebf5b4141f
drbig30 <113547431+drbig30@users.noreply.github.com>
2023-09-03 16:48:19 +0000

    landingpage

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <div className="p-5 container-fluid bg-dark text-white">
+            El mensaje que tu quieras
+        </div>
+    )
+}
+
+export default Footer
